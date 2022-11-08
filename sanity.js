f357c208a633b065333449af7e999e62bee203db
ddboy19912 <99345157+ddboy19912@users.noreply.github.com>
2022-11-08 13:40:25 +0100

    First Commit

---
+++ sanity.js
@@ -0,0 +1,32 @@
+import {
+  createClient,
+  createPortableTextComponent,
+  createPreviewSubscriptionHook,
+} from 'next-sanity';
+
+import createImageUrlBuilder from '@sanity/image-url';
+
+export const config = {
+  /**
+   * Find your project ID and dataset in `sanity.json` in your studio project.
+   * These are considered “public”, but you can use environment variables
+   * if you want differ between local dev and production.
+   *
+   * https://nextjs.org/docs/basic-features/environment-variables
+   **/
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-08-11', // or today's date for latest
+  /**
+   * Set useCdn to `false` if your application require the freshest possible
+   * data always (potentially slightly slower and a bit more expensive).
+   * Authenticated request (like preview) will always bypass the CDN
+   **/
+  useCdn: process.env.NODE_ENV === 'production',
+};
+
+// Client for fetching data in the getProps page func
+export const sanityClient = createClient(config);
+
+// Helper func for gen image urls
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
